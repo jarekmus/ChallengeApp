4fdc5e19209629d333afe9a66aaf4e61c68786e8
jarekmus <jarekmus@icloud.com>
2023-11-22 01:20:29 +0100

    brakuje dwoch plikow

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// zadanie
+string name = "Jarek";
+int age = 40;
+string gen = "male";
+
+if (name == "Jarek" && age < 18)
+{
+    Console.WriteLine("Jarek jest niepelnoletni!");
+}
+else if (age < 18 && gen != "male")
+{
+    Console.WriteLine("Pełnoletnia kobieta");
+}
+else if (name == "Jarek" && age == 40)
+{
+    Console.WriteLine("Jarek, lat 40");
+}
