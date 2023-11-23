82d482e5722f7bbd37de45cbac5cdb9e2417e09c
jarekmus <jarekmus@icloud.com>
2023-11-23 23:00:23 +0100

    Zadanie dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,17 +1,21 @@
-﻿// zadanie
-string name = "Jarek";
-int age = 40;
-string gen = "male";
+﻿// zadanie dzień 5
 
-if (name == "Jarek" && age < 18)
+int number = 5673674;
+string numberAsString = number.ToString();
+char[] chars = numberAsString.ToCharArray();
+Console.WriteLine("W dzisiejszym losowaniu padły następujące liczby");
+
+for (var i = 0; i < 10; i++)
 {
-    Console.WriteLine("Jarek jest niepelnoletni!");
-}
-else if (age < 18 && gen != "male")
-{
-    Console.WriteLine("Pełnoletnia kobieta");
-}
-else if (name == "Jarek" && age == 40)
-{
-    Console.WriteLine("Jarek, lat 40");
+    var counter = 0;
+    foreach (char a in chars)
+    {
+        string iString = i.ToString();
+        string aString = a.ToString();
+        if (aString == iString)
+        {
+            counter++;
+        }
+    }
+    Console.WriteLine(i + " => " + counter);
 }
